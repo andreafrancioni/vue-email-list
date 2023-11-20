079476a238d3a9a25ac51a1b95a0ee25642068f2
andreafrancioni <andreafrancioni.work@gmail.com>
2023-11-20 18:53:16 +0100

    Genera 10 mail in pagina da api

--- js/main.js
+++ js/main.js
@@ -4,10 +4,25 @@ const { createApp } = Vue;
 const opzioni = {
     data: function () {
         return {
-            message: 'Benvenuto',
+            userMail:[]
 
         }
-    }
+    },
+    methods:{
+        generaMail() {
+
+            console.log("Genera mail click")
+            for (let i = 0; i < 10; i++) {
+                
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(resp => {
+                    console.log(resp.data.response);
+    
+                    this.userMail.push(resp.data.response);
+                });
+                
+            }
+        }
+    },
 };
 
 createApp(opzioni).mount('#app')
